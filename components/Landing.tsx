c72072307a9c5757e0d78b60fb58d6ea64f852e3
moh-sa <tno.rappelz@gmail.com>
2023-01-13 01:47:44 +0300

    removed the btns and changed to 13

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -7,17 +7,17 @@ const Landing = () => {
       <div className="space-y-8">
         <h1 className="space-y-3 text-5xl font-semibold tracking-wide lg:text-6xl xl:text-7xl">
           <span className="bgGradiant block bg-clip-text text-transparent">
-            iPhone 14
+            iPhone 13
           </span>
           <span className="block">Big and</span>
           <span className="block text-7xl">Bigger</span>
         </h1>
-        <div className="space-x-8">
-          <Button title="Buy Now" />
-          <a href="#" className="link">
+        {/* <div className="space-x-8">
+          <Button title="Buy Now" isDisabled />
+          <a href="#" className="link cursor-not-allowed">
             Learn More
           </a>
-        </div>
+        </div> */}
       </div>
       <div className="lg;w-[600px] relative hidden h-[450px] w-[700px] transition-all duration-500 md:inline lg:h-[650px]">
         <Image src="/iphone.png" alt="" fill style={{ objectFit: "cover" }} />
