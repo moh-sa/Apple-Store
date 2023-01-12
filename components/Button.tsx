70ce9aa67625bc338dab9a987677e52457a7ebb6
moh-sa <tno.rappelz@gmail.com>
2023-01-13 01:44:06 +0300

    added disabled class

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,14 +5,25 @@ interface Props {
   loading?: boolean;
   padding?: string;
   noIcon?: boolean;
+  isDisabled?: boolean;
 }
 
-const Button = ({ title, width, loading, padding, noIcon, onClick }: Props) => {
+const Button = ({
+  title,
+  width,
+  loading,
+  padding,
+  noIcon,
+  onClick,
+  isDisabled,
+}: Props) => {
   return (
     <button
       className={`ease group relative z-30 box-border inline-flex ${
         width ? width : "w-auto"
-      } ${padding} bgGradiant cursor-pointer items-center justify-center overflow-hidden rounded bg-indigo-600 px-8 py-3 font-bold text-white transition-all duration-300 focus:outline-none`}
+      } ${padding} bgGradiant cursor-pointer items-center justify-center overflow-hidden rounded bg-indigo-600 px-8 py-3 font-bold text-white transition-all duration-300 focus:outline-none ${
+        isDisabled ? "cursor-not-allowed" : ""
+      }`}
       onClick={onClick}
     >
       <span className="absolute bottom-0 right-0 -mb-8 -mr-5 h-20 w-8 translate-x-1 rotate-45 transform bg-white opacity-10 transition-all duration-300 ease-out group-hover:translate-x-0"></span>
